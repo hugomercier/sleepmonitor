cd5051f2ef2173b034db5611a51624a3e2f775ce
Hugo Mercier <hugo.mercier@polytechnique.edu>
2014-02-06 20:49:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sleepmonitor::Application.config.secret_key_base = 'b79c4eea59c7014ab34725af8be51208ac1424a2c202c68ed79960dd21c3af91cc82cfda9fcad2d3aa0f01142ccb0ff45b0b420152565faae23a7c8651c6a8af'
